7d47065eb5c24011f92b3d0c463ea66e96cb5c1c
Christian Asch <casch@cenat.ac.cr>
2023-04-21 08:39:46 -0600

    Tweaked run.sh script

--- configurations/run.sh
+++ configurations/run.sh
@@ -1,12 +1,12 @@
-ID=100
+ID=$1
 
 mpiexec -np 4\
-    /home/casch/Devel/bs-solctra/bs-solctra-implementations/bs_solctra_mpi_openmp/build/main/bs_solctra\
+    ../build/main/bs_solctra\
     -length 1024\
     -particles ../data/input_1000.txt\
     -id $ID\
     -resource ../data/resources/\
-    -steps 10\
+    -steps 100\
     -mode 1\
     -magnetic_prof 0 100 0 2\
     -print_type 1
